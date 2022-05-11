1c5892b6e2e0d7ce624eb665e10870e681dee071
JoeyBanna <84869190+JoeyBanna@users.noreply.github.com>
2022-05-11 12:05:20 +0000

    page link commit

--- js/nav.js
+++ js/nav.js
@@ -3,20 +3,22 @@ const createNav = () => {
 
   nav.innerHTML = `
       <div class="nav">
-          <img src="img/dark-logo.png" class="brand-logo" alt="">
+          <img src="img/dark-logo.png"  class="brand-logo" alt="">
           <div class="nav-items">
               <div class="search">
                   <input type="text" class="search-box" placeholder="search brand, product">
-                  <button class="search-btn">search</button>
+                  <a href="/searchpage.html">
+                  <button class="search-btn"> Search </button>
+                  </a>
               </div>
-              <a href="#"><img src="img/user.png" alt=""></a>
+              <a href="/js/signUp.html"><img src="img/user.png" alt=""></a>
               <a href="#"><img src="img/cart.png" alt=""></a>
           </div>
       </div>
       <ul class="links-container">
-          <li class="link-item"><a href="#" class="link">home</a></li>
-          <li class="link-item"><a href="#" class="link">women</a></li>
-          <li class="link-item"><a href="#" class="link">men</a></li>
+          <li class="link-item"><a href="/index1.html" class="link">home</a></li>
+          <li class="link-item"><a href="/womenscollection.html" class="link">women</a></li>
+          <li class="link-item"><a href="/mencoll.html" class="link">men</a></li>
           <li class="link-item"><a href="#" class="link">kids</a></li>
           <li class="link-item"><a href="#" class="link">accessories</a></li>
       </ul>
